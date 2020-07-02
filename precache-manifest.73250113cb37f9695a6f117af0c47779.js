4b4652612049422b5654fbcb07b0f8f1a9595c1d
Thomas Ostendorf <thomas.ostendorf@orbitlabs.de>
2020-07-02 16:13:28 +0200

    Updates

---
+++ precache-manifest.73250113cb37f9695a6f117af0c47779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011a1bfe38544993c9936a8464d5cf6c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "8792bcdb6b8b05068c15",
+    "url": "/monsters/static/css/main.966f6c0e.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8792bcdb6b8b05068c15",
+    "url": "/monsters/static/js/main.3ff7a874.chunk.js"
+  },
+  {
+    "revision": "28c7cc27b21cfacfd62e",
+    "url": "/monsters/static/js/runtime-main.8d89be33.js"
+  }
+]);
